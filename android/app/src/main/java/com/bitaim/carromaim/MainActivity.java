f266348893ea9b34f56886c623d827bc1258c7ed
abrahambarbhuiya034-dotcom <abrahambarbhuiya034@gmail.com>
2026-05-02 21:03:13 +0530

    Add carrom-aimxassist v8.4 fixed

---
+++ android/app/src/main/java/com/bitaim/carromaim/MainActivity.java
@@ -0,0 +1,24 @@
+package com.bitaim.carromaim;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
+
+public class MainActivity extends ReactActivity {
+
+    @Override
+    protected String getMainComponentName() {
+        // Must match the "name" field in app.json exactly
+        return "AIMxASSIST";
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new DefaultReactActivityDelegate(
+                this,
+                getMainComponentName(),
+                DefaultNewArchitectureEntryPoint.getFabricEnabled()
+        );
+    }
+}
